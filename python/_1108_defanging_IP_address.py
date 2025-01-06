ef95b32e4c2712fa9a66aeaaa9a686a4883fd55c
Suerdo Flaubert <suerdo2015@gmail.com>
2025-01-06 20:26:30 -0300

    Resolution: 1108. Defanging ipv4 address

---
+++ python/_1108_defanging_IP_address.py
@@ -0,0 +1,16 @@
+import re
+
+class Solution:
+    @staticmethod
+    def isIPv4(address: str) -> bool:
+        # Padrão para IPv4 (4 grupos de 1-3 dígitos, separados por '.')
+        ipv4_pattern = r'^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})$'
+        return bool(re.match(ipv4_pattern, address))
+
+    @staticmethod
+    def defangIPaddr(address: str) -> str:
+        if Solution.isIPv4(address):
+            return address.replace(".", "[.]")
+        else:
+            raise ValueError("The provided address is not a valid IPv4 address.")
+        
